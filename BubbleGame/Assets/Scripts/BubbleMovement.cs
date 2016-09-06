f0e6bdc1aaf74adf194f88fd22e2cc862a2a6599
Sarah <sarahmichellemeyer@gmail.com>
2016-09-06 14:45:28 -0600

    adding bubble game unity project

---
+++ BubbleGame/Assets/Scripts/BubbleMovement.cs
@@ -0,0 +1,33 @@
+﻿	using UnityEngine;
+	using System.Collections;
+
+public class BubbleMovement : MonoBehaviour {
+	public Vector3 target;
+
+	public float speed=13f;
+
+	private Rigidbody2D rb2d;
+
+	Vector3 screenPos;
+
+	int direction = 1;
+
+	void  Start (){
+		rb2d = GetComponent<Rigidbody2D> ();
+		target = Random.insideUnitSphere * 5;
+		transform.Rotate(target);
+	}
+	void  Update (){
+		transform.position += (transform.forward * speed * Time.deltaTime)*direction;
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.CompareTag ("Wall")){
+				direction *= -1;
+		}
+		if (other.gameObject.CompareTag ("PickUp")){
+			direction *= -1;
+		}
+	}
+}
+
