e94f6944dfa56214343214743a29bc3c90e34ae7
Sarah <sarahmichellemeyer@gmail.com>
2016-09-06 15:01:21 -0600

    bubbles are not disappearing for no reason

--- BubbleGame/Assets/Scripts/BubbleMovement.cs
+++ BubbleGame/Assets/Scripts/BubbleMovement.cs
@@ -12,18 +12,18 @@ public class BubbleMovement : MonoBehaviour {
 
 	int direction = 1;
 
-	void  Start (){
+	void Start (){
 		rb2d = GetComponent<Rigidbody2D> ();
 		target = Random.insideUnitSphere * 5;
 		transform.Rotate(target);
 	}
-	void  Update (){
-		transform.position += (transform.forward * speed * Time.deltaTime)*direction;
+	void FixedUpdate (){
+		rb2d.MovePosition((Vector3)rb2d.position + (transform.forward * speed * Time.deltaTime * direction));
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.CompareTag ("Wall")){
-				direction *= -1;
+			direction *= -1;
 		}
 		if (other.gameObject.CompareTag ("PickUp")){
 			direction *= -1;
