9ccc464b2d404acac081da03fd1995f3f4274559
Devin Stone <devstone11@gmail.com>
2016-09-07 09:46:09 -0600

    pause the game with button instead of 'p'

--- BubbleGame/Assets/Scripts/UIManager.cs
+++ BubbleGame/Assets/Scripts/UIManager.cs
@@ -4,32 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 	GameObject[] pauseObjects;
+	GameObject[] playObjects;
 
 	// Use this for initialization
 	void Start () {
 		Time.timeScale = 1;
 		pauseObjects = GameObject.FindGameObjectsWithTag ("ShowOnPause");
+		playObjects = GameObject.FindGameObjectsWithTag ("HideOnPause");
 		hidePaused ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//uses the p button to pause and unpause the game
-		if (Input.GetKeyDown (KeyCode.P)) 
-		{
-			if (Time.timeScale == 1) {
-				Time.timeScale = 0;
-				showPaused ();
-			} else if (Time.timeScale == 0) {
-				Debug.Log ("high");
-				Time.timeScale = 1;
-				hidePaused ();
-			}
-		}
+	}
+
+	public void pause() {
+		Time.timeScale = 0;
+		showPaused ();
 	}
 
 	public void resume() {
-		Debug.Log ("hello there");
 		Time.timeScale = 1;
 		hidePaused ();
 	}
@@ -39,12 +33,18 @@ public class UIManager : MonoBehaviour {
 		foreach (GameObject g in pauseObjects) {
 			g.SetActive (true);
 		}
+		foreach (GameObject g in playObjects) {
+			g.SetActive (false);
+		}
 	}
 
 	//hides objects with ShowOnPause tag
 	public void hidePaused() {
 		foreach (GameObject g in pauseObjects) {
 			g.SetActive (false);
+		}
+		foreach (GameObject g in playObjects) {
+			g.SetActive (true);
 		}
 	}
 
