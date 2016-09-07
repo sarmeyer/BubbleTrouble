526043dc81d08fdad3b3f402a39cf321e2531dbe
Sarah <sarahmichellemeyer@gmail.com>
2016-09-07 14:32:44 -0600

    timeout page, game timer

---
+++ BubbleGame/Assets/Scripts/BubbleBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BubbleBehavior : MonoBehaviour {
+	private bool mouseEntered;
+
+	void Start (){
+	}
+	void OnMouseEnter(){
+		mouseEntered = true;
+	}
+
+	void OnMouseExit(){
+		mouseEntered = false;
+	}
+	void Update() {
+		if (mouseEntered && Input.GetMouseButtonDown (0)) {
+			this.gameObject.SetActive (false);
+		}
+	}
+}
